daa24ef9e6c5bdc1f411e9b0abece51875215a2d
Jakub@Lap <kuba@zarembski.com>
2020-04-02 20:28:24 +0200

    to many strings

--- src/main/java/com/zarembski/covid/requests/HerokuappApiRequest.java
+++ src/main/java/com/zarembski/covid/requests/HerokuappApiRequest.java
@@ -5,7 +5,6 @@ import com.zarembski.covid.service.CovidDataService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.client.Client;
